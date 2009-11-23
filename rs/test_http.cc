9732cac9ba036c06894ea4ed2c364f6625c8132a
Piotr Truszkowski <truszkowski@gmail.com>
2009-11-23 14:27:20 +0100

    Test pobierania wielu stron na jednym obiekcie http(ale sekwencyjnie)

--- rs/test_http.cc
+++ rs/test_http.cc
@@ -6,30 +6,31 @@ int main(int argc, char **argv)
 {
   Http http;
   char *page = NULL;
-  size_t len = 0;
+  size_t size = 0;
 
   for (int i = 1; i < argc; ++i) {
-    cerr << "== '" << argv[i] << "' ==" << endl;
+    cerr << "== GET '" << argv[i] << "' ==" << endl;
+    
     try {
       //http.set_verbose(true);
-      int code = http.get(page, len, argv[i]);
+      int code = http.get(page, size, argv[i]);
       if (page) delete[] page;
 
       const Http::Headers &hdrs = http.get_headers();
 
       Http::Headers::const_iterator it;
-      const char *loc = http.get_recv_redirect();
-      const char *coo = http.get_recv_cookies();
+      const char *loc = http.get_recv_header("location");
+      const char *coo = http.get_recv_header("set-cookie");
       const char *len = http.get_recv_header("content-length");
     
-      cerr << "= code = " << code << endl;
-      cerr << "= headers size = " << hdrs.size() << endl;
-      cerr << "= location = " << (loc?loc:"?") << endl;
-      cerr << "= set-cookie = " << (coo?coo:"?") << endl;
-      cerr << "= content-length = " << (len?len:"?") << " (plik: " << len << ")"<< endl;
+      cerr << "== code = " << code << endl;
+      cerr << "== headers size = " << hdrs.size() << endl;
+      cerr << "== location = " << (loc?loc:"?") << endl;
+      cerr << "== set-cookie = " << (coo?coo:"?") << endl;
+      cerr << "== content-length = " << (len?len:"?") << " (strona: " << size << ")"<< endl;
     } 
     catch (Exception &e) {
-      cerr << "= exception: " << e.what() << endl;
+      cerr << "== exception: " << e.what() << endl;
     }
   }
 
